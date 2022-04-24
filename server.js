d40f09f1bfd6f45db78506cd9a15832da0e4a343
zadigg <62830716+zadigg@users.noreply.github.com>
2022-04-25 00:02:04 +0300

    Sidehustle backend

--- server.js
+++ server.js
@@ -1,75 +1,71 @@
 //Importing modules http, os, fs
-const http = require('http');
-const os = require('os');
-const fs = require('fs');
+const http = require("http");
+const os = require("os");
+const fs = require("fs");
 
 //Initializing port
 const port = 4000;
+let osinfo = {
+  hostname: os.hostname(),
+  platform: os.platform(),
+  architecture: os.arch(),
+  numberOfCPUS: os.cpus().length,
+  networkInterfaces: os.networkInterfaces(),
+  uptime: os.uptime(),
+};
 
+const osinfoData = JSON.stringify(osinfo);
 //defining server
-const server = http.createServer((req,res) => {
-    const url = req.url;
+const server = http.createServer((req, res) => {
+  const url = req.url;
 
-    if(url === '/'){
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        fs.readFile('pages/index.html', (err,data) => {
-            if(err){
-                res.statusCode = 404;
-                res.write('File not found');
-            } else{
-                res.write(data);
-            } 
-        
-            res.end();
-
-        });
-        
-    } else if(url === '/about'){
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        fs.readFile('pages/about.html', (err,data) => {
-            res.write(data);
-            res.end();
-        });
-    } else if (url === '/sys'){
-        res.statusCode = 201;
-        res.setHeader('Content-Type', 'text/plain');
-        res.write('Your OS info has been saved successfully');
-        res.end()
-     }else {
+  if (url === "/") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    fs.readFile("pages/index.html", (err, data) => {
+      if (err) {
         res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/html');
-        fs.readFile('pages/404.html', (err,data) => {
-            res.write(data);
-            res.end();
-        });
-    }
-});
+        res.write("File not found");
+      } else {
+        res.write(data);
+      }
 
+      res.end();
+    });
+  } else if (url === "/about") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    fs.readFile("pages/about.html", (err, data) => {
+      res.write(data);
+      res.end();
+    });
+  } else if (url === "/sys") {
+    res.statusCode = 201;
+    res.setHeader("Content-Type", "text/plain");
+    fs.writeFile("osinfo.json", osinfoData, "utf8", (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("osinjo.json is saved successfully");
+      }
+    });
+    res.write("Your OS info has been saved successfully");
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/html");
+    fs.readFile("pages/404.html", (err, data) => {
+      res.write(data);
+      res.end();
+    });
+  }
+});
 
 // Starting server
 server.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
 
-let osinfo = {
-    hostname: os.hostname(),
-    platform: os.platform(),
-    architecture: os.arch(),
-    numberOfCPUS: os.cpus().length,
-    networkInterfaces: os.networkInterfaces(),
-    uptime: os.uptime(),
-};
 // console.log(osinfo);
 
-const osinfoData = JSON.stringify(osinfo);
-console.log(osinfoData);
-
-fs.writeFile('osinfo.json', osinfoData, 'utf8', (err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('osinjo.json is saved successfully');
-    }
-});
+// console.log(osinfoData);
